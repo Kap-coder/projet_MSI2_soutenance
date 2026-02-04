1926f4e19b5607a0b74f8e1af866fe76d2aa688f
Kap-coder <angekapel007@gmail.com>
2026-02-04 15:02:19 +0100

    feat: Ajout des commandes `makemigrations` et `migrate` au script de démarrage.

--- run.bat
+++ run.bat
@@ -3,6 +3,8 @@ echo Activation de l'environnement virtuel...
 call .\venv\Scripts\activate.bat
 
 echo Lancement du serveur Django...
+python manage.py makemigrations
+python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
 
 pause
